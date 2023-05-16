be833c159b2a48e9f1673abbbf4787eabfffb833
adam bona <adambona6@gmail.com>
2023-05-16 19:45:31 +1000

    Folder structure and base/index working

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,5 @@
 #from package import Class
-from flask import Flask 
+from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -9,24 +9,24 @@ db=SQLAlchemy()
 #create a function that creates a web application
 # a web server will run this web application
 def create_app():
-  
+
     app=Flask(__name__)  # this is the name of the module/package that is calling this app
     app.debug=True
     app.secret_key='somesecretgoeshere'
     #set the app configuration data 
-    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///mydbname.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///brisbane_live_events.sqlite'
     #initialise db with flask app
     db.init_app(app)
 
     bootstrap = Bootstrap5(app)
     
     #initialize the login manager
-    login_manager = LoginManager()
+    # login_manager = LoginManager()
     
     #set the name of the login function that lets user login
     # in our case it is auth.login (blueprintname.viewfunction name)
-    login_manager.login_view='auth.login'
-    login_manager.init_app(app)
+    # login_manager.login_view='auth.login'
+    # login_manager.init_app(app)
 
     #create a user loader function takes userid and returns User
     #from .models import User  # importing here to avoid circular references
@@ -43,6 +43,3 @@ def create_app():
     app.register_blueprint(auth.bp)
     
     return app
-
-
-
