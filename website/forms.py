693dffd5e857a3fa88ad45798e138179d51f4069
Lets-Go-Champ <christian.hiley@gmail.com>
2023-06-06 21:41:35 +1000

    create event validation

--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm, Form
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField, TimeField, IntegerField, DateField, RadioField, BooleanField, FormField, FieldList
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField, TimeField, IntegerField, DateField, RadioField, BooleanField, FormField, FieldList, FloatField
 from datetime import datetime, date
 from wtforms.validators import InputRequired, Length, Email, EqualTo, NumberRange, Regexp, ValidationError
 from .models import User
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 from flask import url_for, Markup
 from werkzeug.security import check_password_hash
+from datetime import timedelta
 
 ALLOWED_FILE = ['PNG','JPG','png','jpg', 'jpeg', 'JPEG']
 
@@ -13,6 +14,7 @@ PASSWORD_REGEX="^.*(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z\\d\\s:]
 PASSWORD_ERROR_MESSAGE="Password must be a text string of arbitrary length, at least 8 non-white-space characters which must contain at least one uppercase letter, one lowercase letter, one digit, one non-alphanumeric character (e.g., !@#$%^&*)"
 
 MOBILE_NUM_REGEX = "^(\+?61|0)[2-478](?:[ -]?[0-9]){8}$"
+#trim space regex
 MOBILE_ERROR_MESSAGE="Mobile number must be a valid Australian mobile number. It must be in the form of 04XXXXXXXX or +61XXXXXXXXX (You can use spaces or hypens to seperate the digits eg. 04XX XXX XXX +61XX-XXX-XXX)."
 
 #custom length validator
@@ -74,7 +76,6 @@ class RegisterForm(FlaskForm):
 
 class createEventForm(FlaskForm):
 
-    
     genre = SelectField("Genre", choices=[
         ("Pop", "Pop"),
         ("DanceEDM", "Dance/EDM"),
@@ -97,7 +98,6 @@ class createEventForm(FlaskForm):
     name = StringField("Event Name", validators=[InputRequired(message="Please enter a name for the event"), validate_length(min=2, max=100, min_error_message='Event name must be 2 characters or greater', max_error_message='Event name must be 100 characters or less')])
     artist_names = StringField("Artists", validators=[InputRequired(message="Please enter the artist/s name")])
 
-
     description = TextAreaField("Detailed Description of the Event", validators=[
         InputRequired(message="Please enter a description for the event."),
         validate_length(min=6, max=500, min_error_message="Description must be 6 characters or greater.",
@@ -106,19 +106,19 @@ class createEventForm(FlaskForm):
 
     image = FileField("Thumbnail Image for the Event", validators=[
         FileRequired("Please upload an image for the event."),
-        FileAllowed(ALLOWED_FILE)
+        FileAllowed(ALLOWED_FILE, message="File type must be either PNG or JPG")
     ])
 
-    venue_name = StringField("Venue Name", validators=[InputRequired(message="Please enter a venue name.")])
+    venue_name = StringField("Venue Name", validators=[InputRequired(message="Please enter a venue name."), validate_length(min=1, max=100, min_error_message='Venue name must be at least 1 character.',max_error_message='Venue name must be 100 characters or less.')])
     street_address = StringField("Street Address", validators=[InputRequired(message="Please enter a street address.")])
 
-    start_date = DateField("", validators=[InputRequired()])
-    end_date = DateField("Event Ends", validators=[InputRequired()])
-    start_time = TimeField("Start Time")
-    end_time = TimeField("End Time")
+    start_date = DateField("Event Starts", validators=[InputRequired(message="Please enter a start date.")])
+    end_date = DateField("Event Ends", validators=[InputRequired(message="Please enter an end date.")])
+    start_time = TimeField("Start Time", validators=[InputRequired(message="Please enter a start time.")])
+    end_time = TimeField("End Time", validators=[InputRequired(message="Please enter an end time.")])
 
 
-    ticket_price = StringField("Price per Ticket", validators=[InputRequired(message="enter ticket price")])
+    ticket_price = FloatField("Price per Ticket", validators=[InputRequired(message="enter ticket price")])
 
     num_tickets = IntegerField("Total Number of Tickets Available", validators=[InputRequired()])
 
@@ -131,10 +131,17 @@ class createEventForm(FlaskForm):
 
     submit=SubmitField("Create Event")
 
-    def validate_start_date(self, event_date):
-        if event_date.data < date.today():
-            raise ValidationError('Date must be in the future')
+    def validate_start_date(self, field):
+        start = field.data
+        if start < date.today():
+            raise ValidationError('The event start date must be in the future.')
 
+    def validate_end_time(self, field):
+        start_time = self.start_time.data
+        end_time = field.data
+        min_end_time = (start_time + timedelta(hours=1)) % timedelta(days=1)
+        if end_time < min_end_time:
+            raise ValidationError('The event end time must be at least 1 hour after the event start time.')
     
 class orderForm(FlaskForm):
     num_tickets=IntegerField("Number of tickets", validators=[InputRequired(), NumberRange(min=1,max=5)])
