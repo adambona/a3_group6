c1845c686e79dc1e6b0982a98ef3c0cfa6b90c8f
Lets-Go-Champ <christian.hiley@gmail.com>
2023-06-07 17:27:20 +1000

    fixed formatting

--- website/forms.py
+++ website/forms.py
@@ -15,15 +15,14 @@ PASSWORD_REGEX="^.*(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z\\d\\s:]
 PASSWORD_ERROR_MESSAGE="Password must be a text string of arbitrary length, at least 8 non-white-space characters which must contain at least one uppercase letter, one lowercase letter, one digit, one non-alphanumeric character (e.g., !@#$%^&*)"
 
 MOBILE_NUM_REGEX = "^(\+?61|0)[2-478](?:[ -]?[0-9]){8}$"
-#trim space regex
 MOBILE_ERROR_MESSAGE="Mobile number must be a valid Australian mobile number. It must be in the form of 04XXXXXXXX or +61XXXXXXXXX (You can use spaces or hypens to seperate the digits eg. 04XX XXX XXX +61XX-XXX-XXX)."
 
 #custom length validator
 def validate_length(min=-1,max=-1, min_error_message="Field length too short", max_error_message="Field length too long"):
   def _validate_length(form, field):
-    if len(field.data) < min :
+    if len(field.data.strip()) < min :
         raise ValidationError(min_error_message) 
-    elif len(field.data) > max:
+    elif len(field.data.strip()) > max:
         raise ValidationError(max_error_message)   
   return _validate_length   
 
@@ -64,11 +63,11 @@ class LoginForm(FlaskForm):
   
  # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired(message="Please enter a user name."), validate_length(min=1, max=36, min_error_message='User name must be at least 1 character.',max_error_message='User name must be 36 characters or less.')])
+    user_name = StringField("User Name", validators=[InputRequired(message="Please enter a user name."), validate_length(min=1, max=36, min_error_message='User name must be at least 1 character.', max_error_message='User name must be 36 characters or less.')])
     email_id = StringField("Email Address", validators=[Email(message="Please enter a valid email address."), Length(min=1,max=254), InputRequired(message="Please enter an email address."), validate_unique_email])
     mobile_number = StringField("Mobile Number", validators=[InputRequired(message="Please enter a mobile number."), Regexp(MOBILE_NUM_REGEX, message=MOBILE_ERROR_MESSAGE)])
     #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(message="Please enter a password."),
+    password = PasswordField("Password", validators=[InputRequired(message="Please enter a password."),
                   EqualTo('confirm', message="Passwords do not match. Please Try again."), Regexp(PASSWORD_REGEX, message=PASSWORD_ERROR_MESSAGE), Length(max=254, message="Password must be 254 characters or less.")])
     confirm = PasswordField("Confirm Password", validators=[InputRequired(message="Please confirm your password.")])
 
@@ -159,7 +158,7 @@ class createEventForm(FlaskForm):
         if end_datetime < min_end_time:
             raise ValidationError('The event end time must be at least 1 hour after the event start time.')
 
-def validate_expiration(form, field):
+    def validate_expiration(form, field):
         data = field.data
 
         if not re.match('^(0[1-9]|1[0-2])\/(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9])$', data):
