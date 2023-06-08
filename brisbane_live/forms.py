9f183a24a0015b9845002107406c9703f3ca94b2
adambona <n9467777@qut.edu.au>
2023-06-08 10:00:19 +1000

    added min number for ticketprice

--- brisbane_live/forms.py
+++ brisbane_live/forms.py
@@ -117,7 +117,7 @@ class createEventForm(FlaskForm):
     end_time = TimeField("End Time", validators=[InputRequired(message="Please enter an end time.")])
 
 
-    ticket_price = FloatField("Price per Ticket", validators=[InputRequired(message="Please enter ticket price")])
+    ticket_price = FloatField("Price per Ticket", validators=[InputRequired(message="Please enter ticket price") , NumberRange(min=1)])
 
     num_tickets = IntegerField("Total Number of Tickets Available", validators=[InputRequired("Please enter the number of tickets."), NumberRange(min=0, message="Number of tickets must be a non-negative integer.")])
 
